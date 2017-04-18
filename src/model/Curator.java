5c2842b1d5fb2c8191abe3a56450ef7b5e43648c
TarielR <tar.iel.reg@gmail.com>
2017-04-18 13:06:49 +0300

    Изменил тип educationYear на int поменял тип в db

--- src/model/Curator.java
+++ src/model/Curator.java
@@ -1,13 +1,21 @@
 package model;
 
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Curator extends Person {
     private String rang;
 
+    public Curator() { }
+
+    public Curator(ResultSet rs, String rang) throws SQLException {
+        super(rs);
+        setRang(rs.getString(3));
+    }
     public String getRang() {return rang; }
 
     public void setRang(String rang) { this.rang = rang; }
-
 
     @Override
     public String toString() {
